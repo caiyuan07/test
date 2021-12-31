61be5b7d77dfb96b85f2e0933a4bfe98d5f3a2e1
caiyuan07 <96829672+caiyuan07@users.noreply.github.com>
2021-12-31 22:46:53 +0800

    添加整形加法运算
    在这里利用一个void add(int a, int b, int *c)函数添加加法功能。

--- test.c
+++ test.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 
+void add(int a, int b, int *c);
+
 int main()
 {
+    int a = 3;
+    int b = 4;
+    int c;
+    add(a, b, &c);
+    printf("a + b = %d + %d = %d\n", a, b, *c);
     printf("hello world!\n");
     return 0;
+}
+
+void add(int a, int b, int *c)
+{
+    *c = a + b;
 }
